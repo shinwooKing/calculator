ca555d60187348d678b5453c635046e2a09b5197
shinwooKing <tlsdnrns@gmail.com>
2023-03-14 02:33:15 +0900

    Revert "Add lbl1 in ui.py to print current date"
    This reverts commit ab2c579f120cdbfa2ec81d50aad6c79ee5d74f80.

--- ui.py
+++ ui.py
@@ -1,15 +1,12 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QHBoxLayout, QPlainTextEdit, QLabel)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,QHBoxLayout, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QDate, Qt
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate()
         self.initUI()
 
     def activateMessage(self):
@@ -23,7 +20,6 @@ class View(QWidget):
         self.te1.setReadOnly(True)
         self.btn1 = QPushButton('Message',self)
         self.btn2 = QPushButton('Clear',self)
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate),self)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
@@ -33,7 +29,7 @@ class View(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl1)
+        vbox.addStretch(1)
 
         self.setLayout(vbox)
         self.setWindowTitle('Calculator')
